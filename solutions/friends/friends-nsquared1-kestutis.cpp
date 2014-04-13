baa7deb2cd3273d33e8cfc67a8b88f6210b6f29a
kestas <kesvil@gmail.com>
2014-04-13 18:54:23 +0100

    n^2 with 2k+1 symbol count hack

---
+++ solutions/friends/friends-nsquared1-kestutis.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define MaxN 100010
+
+string S;
+vector<string> ANS;
+int N;
+int C[1000] = {0};
+
+int main() {
+	cin >> S;
+	
+	for (int i = 0; i < S.size(); i++)
+		C[S[i]]++;
+		
+	N = S.length() / 2;
+	
+	for (int i = 0; i < S.size(); i++) if (C[S[i]] & 1) {
+		string Sn = S.substr(0, i) + S.substr(i + 1, 2*N - i);
+		bool good = true;
+		for (int i = 0; i < N; i++) 
+			if (Sn[i] != Sn[i+N])
+				good = false;
+		if (good)
+			ANS.push_back(Sn.substr(0, N));
+	}
+
+	bool good = true;
+	for (int i = 1; i < ANS.size(); i++)
+		if (ANS[0] != ANS[i])
+			good = false;
+	if (good) 
+		printf("%s\n", ANS[0].c_str());
+	else 
+		printf("NOT UNIQUE\n");
+
+
+	return 0;
+}
