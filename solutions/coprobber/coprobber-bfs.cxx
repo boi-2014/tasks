cafc83b47d850b7475270738a1b0dc376ddddab0
Marijonas <marpetr@gmail.com>
2014-04-13 19:23:21 +0100

    Finished the official solution for cop and robber

---
+++ solutions/coprobber/coprobber-bfs.cxx
@@ -0,0 +1,68 @@
+#include "coprobber.h"
+#include <queue>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef tuple<int, int, int> Position;
+
+const int COP = 0, ROBBER = 1;
+vector<int> Neighbours[MAX_N];
+int LeftToWin[2][MAX_N][MAX_N];
+int NextPos[MAX_N][MAX_N];
+
+int start(int N, bool A[MAX_N][MAX_N]) {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (A[i][j])
+				Neighbours[i].push_back(j);
+
+	for (int c = 0; c < N; c++)
+		for (int r = 0; r < N; r++)
+			if (c != r) {
+				LeftToWin[COP][c][r] = 1;
+				LeftToWin[ROBBER][c][r] = Neighbours[r].size();
+			}
+
+	queue<Position> q;
+	for (int i = 0; i < N; i++) {
+		q.push(Position(COP, i, i));
+		q.push(Position(ROBBER, i, i));
+	}
+	
+	int leftUntilWin = 2 * N * N;
+	while (!q.empty()) {
+		int t, c, r;
+		tie(t, c, r) = q.front();
+		q.pop();
+		leftUntilWin--;
+		if (t == COP) {
+			for (int n : Neighbours[r])
+				if (c != n && LeftToWin[ROBBER][c][n] > 0
+						&& (--LeftToWin[ROBBER][c][n]) == 0)
+					q.push(Position(ROBBER, c, n));
+		} else if (t == ROBBER) {
+			for (int n : Neighbours[c])
+				if (r != n && LeftToWin[COP][n][r] > 0
+						&& (--LeftToWin[COP][n][r]) == 0) {
+					q.push(Position(COP, n, r));
+					NextPos[n][r] = c;
+				}
+			if (LeftToWin[COP][c][r] > 0
+					&& (--LeftToWin[COP][c][r]) == 0) {
+				q.push(Position(COP, c, r));
+				NextPos[c][r] = c;
+			}
+		}
+	}
+	
+	return leftUntilWin == 0 ? 0 : -1;
+}
+
+int cop = 0;
+
+int nextMove(int robber) {
+	cop = NextPos[cop][robber];
+	return cop;
+}
