ab8c6c453c7ada7b535a62a830ec89bb87549b1a
Donatas <donce.lt@gmail.com>
2014-04-24 14:31:20 +0300

    Sequence test generator

--- testdata/sequence/generate.cpp
+++ testdata/sequence/generate.cpp
@@ -3,42 +3,37 @@
 #include <fstream>
 #include <vector>
 
-const char *TESTS_DIR = "";
+using namespace std;
 
-const char *TEMP_OUTPUT = "temp_output";
+typedef long long ll;
+
+const ll MAX_ANSWER = 102345678900000LL;
+
+
+const char *TEMP_OUTPUT = "temp_output";//TODO: remove this file after
 
 const char *SOLUTIONS_ROOT = "../../solutions/sequence/";
 const int SOLUTIONS_COUNT = 3;
 const char *SOLUTIONS[SOLUTIONS_COUNT] = {
-	"sequence-bruteforce-donatas",
 	"sequence_OK_Rihards",
 	"sequence-daumilas",
+	"sequence-bruteforce-donatas",
 	//"sequence-donatas",
-	//"sequence-KlogK-vytautas", //needs fixing
+	//"sequence-KlogK-vytautas",
+};
+const int SOLUTIONS_START_NUMBER[SOLUTIONS_COUNT] = {
+	0,
+	0,
+	0,
 };
 
 const int SUBTASKS_COUNT = 5;
 
-const int GROUP_A_NR = 0;
-const int GROUP_A_MAX_K = 1000;
-const int GROUP_A_MAX_N = 1000;
-
-const int GROUP_B_NR = 1;
-const int GROUP_B_MAX_K = 100000;
-
-
-
-const int sample_test[] = { 7, 8, 9, 5, 1, 2 };
-const int static_test[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-
-
-using namespace std;
 
 class Test {
 public:
 	vector<int> sequence;
-	long long answer;
+	ll answer;
 
 	Test() {
 		answer = -1;
@@ -74,6 +69,15 @@ public:
 	}
 };
 
+
+//TODO: remove
+const int sample_test[] = { 7, 8, 9, 5, 1, 2 };
+const int static_test[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+vector<Test> tests[SUBTASKS_COUNT];
+
+
 void makeTestFile(string filename, Test test) {
 	FILE *file = fopen(filename.c_str(), "w");
 	if (file == NULL)
@@ -83,7 +87,7 @@ void makeTestFile(string filename, Test test) {
 	fclose(file);
 }
 
-string toString(int number) {
+string toString(ll number) {
 	string s = "";
 	while (number) {
 		int digit = number % 10;
@@ -93,7 +97,7 @@ string toString(int number) {
 	return s;
 }
 
-Test generateTest(const int k, const int n) {
+Test generateTest(const ll k, const ll n) {
 	Test test;
 	for (int i = 0; i < k; ++i) {
 		string s = toString(n + i);
@@ -105,12 +109,21 @@ Test generateTest(const int k, const int n) {
 
 }
 
-Test generateRandomTest(const int MAX_K, const int MAX_N=-1) {
-	int k = rand() % MAX_K;
-	int n = rand();
-	if (MAX_N != -1)
-		n %= MAX_N;
+Test generateRandomTest(const ll maxK, const ll maxN=MAX_ANSWER) {
+	ll k = rand() % (maxK+1);
+	ll n = rand() % (maxN+1);
 	return generateTest(k, n);
+}
+
+Test generateOneDigitRandomTest(const ll MAX_K) {
+	Test test;
+	int digit = rand() % 10;
+	int k = rand() % (MAX_K + 1);
+	cout << k << " x \"" << digit << "\"" << endl;
+	for (int i = 0; i < k; ++i) {
+		test.add(digit);
+	}
+	return test;
 }
 
 Test makeStaticTest(const int sequence[], int answer=-1) {
@@ -119,27 +132,26 @@ Test makeStaticTest(const int sequence[], int answer=-1) {
 	return test;
 }
 
-vector<Test> tests[SUBTASKS_COUNT];
 
 void addTest(int subtask, Test test) {
-	tests[subtask].push_back(test);
+	tests[subtask-1].push_back(test);
 }
 
-long long readAnswer(const char *filename) {
-	long long answer;
+ll readAnswer(const char *filename) {
+	ll answer;
 	FILE *file = fopen(filename, "r");
 	fscanf(file, "%lld", &answer);
 	fclose(file);
 	return answer;
 }
 
-void writeAnswer(long long answer, const char *filename) {
+void writeAnswer(ll answer, const char *filename) {
 	FILE *file = fopen(filename, "w");
 	fprintf(file, "%lld\n", answer);
 	fclose(file);
 }
 
-bool contains(long long number, int digit) {
+bool contains(ll number, int digit) {
 	while (number != 0) {
 		if (number % 10 == digit)
 			return true;
@@ -149,8 +161,8 @@ bool contains(long long number, int digit) {
 }
 
 //if answer is possible (not necessary smallest)
-bool isCorrect(Test test, long long answer) {
-	for (long long i = 0; i < test.sequence.size(); ++i) {
+bool isCorrect(Test test, ll answer) {
+	for (ll i = 0; i < test.sequence.size(); ++i) {
 		if (!contains(answer + i, test.sequence[i])) {
 			return false;
 		}
@@ -158,28 +170,61 @@ bool isCorrect(Test test, long long answer) {
 	return true;
 }
 
+ll testFile(const char *inputFilename, Test test) {
+	char command[1024];
+	ll answer = test.answer;
+	for (int k = 0; k < SOLUTIONS_COUNT; ++k) {
+		string solution = string(SOLUTIONS_ROOT) + string(SOLUTIONS[k]);
+		sprintf(command, "%s < %s > %s", solution.c_str(), inputFilename, TEMP_OUTPUT);
+		system(command);
+		ll now = readAnswer(TEMP_OUTPUT);
+		bool correct;
+		if (answer != -1 && now == answer) {
+			//the same answer
+			correct = true;
+		}
+		else if (answer == -1 || (answer != -1 && now < answer)) {
+			//check if new better answer is correct
+			correct = isCorrect(test, now);
+			if (correct)
+				answer = now;
+		} 
+		cout << (correct ? "+" : "-") << now << " by " << SOLUTIONS[k] << endl;
+		if (!correct)
+			cout << "----------------------------------------------------------incorrect" << endl;
+	}
+	if (answer == -1) {
+		cout << "No correct solution found" << endl;
+		throw "No correct solution found";
+	}
+	cout << "Answer: " << answer << endl;
+	return answer;
+}
+
 int main() {
+	srand(time(0));
 	try {
 		cout << "Generating tests..." << endl;
 		//generate tests
-		//addTest(0, makeStaticTest(sample_test));
-		//for (int i = 1; i < 1000; ++i) {
-			//addTest(0, generateTest(100, i));
-		//}
+		//#1: N, K <= 1000
 		for (int i = 0; i < 10; ++i)
-			addTest(GROUP_A_NR, generateRandomTest(GROUP_A_MAX_K, GROUP_A_MAX_N));
-		//for (int i = 0; i < 10; ++i)
-			//addTest(GROUP_B_NR, generateRandomTest(GROUP_B_MAX_K));
-		//for (int i = 1; i < 100; ++i)
-			//addTest(0, generateTest(10, i));
-
+			addTest(1, generateRandomTest(1000, 1000));
+		//#2: K <= 1000
+		for (int i = 0; i < 10; ++i)
+			addTest(2, generateRandomTest(1000));
+		//#3: all digits are equal
+		for (int i = 0; i < 10; ++i)
+			addTest(1, generateOneDigitRandomTest(1000));
+		//#4: K <= 100 000
+		for (int i = 0; i < 10; ++i)
+			addTest(4, generateRandomTest(100000));
 
 		//make tests
 		for (int i = 0; i < SUBTASKS_COUNT; ++i) {
 			for (int j = 0; j < tests[i].size(); ++j) {
 				cout << "--------------" << endl;
 				char buffer[255];
-				sprintf(buffer, "%ssequence.%d-%02d", TESTS_DIR, i, j);
+				sprintf(buffer, "sequence.%d-%02d", i+1, SOLUTIONS_START_NUMBER[i]+j);
 				string inputFile = string(buffer) + ".in";
 				string outputFile = string(buffer) + ".sol";
 				cout << "Supposed answer: " << tests[i][j].answer << endl;
@@ -187,33 +232,7 @@ int main() {
 				makeTestFile(inputFile, tests[i][j]);
 
 				cout << outputFile << endl;
-				char command[1024];
-				long long answer = tests[i][j].answer;
-				for (int k = 0; k < SOLUTIONS_COUNT; ++k) {
-					string solution = string(SOLUTIONS_ROOT) + string(SOLUTIONS[k]);
-					sprintf(command, "%s < %s > %s", solution.c_str(), inputFile.c_str(), TEMP_OUTPUT);
-					system(command);
-					long long now = readAnswer(TEMP_OUTPUT);
-					bool correct;
-					if (answer != -1 && now == answer) {
-						//the same answer
-						correct = true;
-					}
-					else if (answer == -1 || (answer != -1 && now < answer)) {
-						//check if new better answer is correct
-						correct = isCorrect(tests[i][j], now);
-						if (correct)
-							answer = now;
-					} 
-					cout << (correct ? "+" : "-") << now << " by " << SOLUTIONS[k] << endl;
-					if (!correct)
-						cout << "----------------------------------------------------------incorrect" << endl;
-				}
-				if (answer == -1) {
-					cout << "No correct solution found" << endl;
-					throw "No correct solution found";
-				}
-				cout << "Answer: " << answer << endl;
+				ll answer = testFile(inputFile.c_str(), tests[i][j]);
 				writeAnswer(answer, outputFile.c_str());
 			}
 		}
