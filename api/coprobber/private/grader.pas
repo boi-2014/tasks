b9199dce55648ce903c916c4f3eaada3ea2d09de
Daumilas <daumilas@gmail.com>
2014-04-27 00:03:19 +0300

    Coprobber - updated pascal grader

--- api/coprobber/private/grader.pas
+++ api/coprobber/private/grader.pas
@@ -9,21 +9,39 @@ program grader;
     VisitedPositions: Matrix;
     
     c, r, t, copCorner, robberCorner, nextCopCorner: Integer;
-  
+    fin, fout : text;
+    
+procedure OutputResult(status, msg: string);
 begin
-  Read(N);
+  //assign (fout, SECRET_OUT);
+  rewrite (fout);
+  WriteLn(fout, status);
+  if (msg <> '') then
+    WriteLn(fout, msg);
+  close (fout);
+  Halt;
+end;
+
+begin
+  assign (fin, 'oprobber-i3hfiehflasil.in');
+  assign (fout, 'oprobber-i3hfiehflasil.out');
+  rewrite (fout);
+  close (fout);
+  reset(fin);
+  Read(fin, N);
   for c := 0 to N-1 do
     for r := 0 to N-1 do
     begin
-      Read(t);
+      Read(fin, t);
       A[c, r] := t = 1;
     end;
-  Read(t);
+  Read(fin, t);
   CopCanWin := t = 1;
   if CopCanWin then
     for c := 0 to N-1 do
       for r := 0 to N do
-        Read(RobberStrategy[c, r]);
+        Read(fin, RobberStrategy[c, r]);
+  close(fin);
   for c := 0 to N-1 do
     for r := 0 to N-1 do
       VisitedPositions[c, r] := False;
@@ -31,39 +49,21 @@ begin
   copCorner := Start(N, A);
   
   if (copCorner <> -1) and not CopCanWin then
-  begin
-    WriteLn('FAIL');
-    WriteLn('Cop cannot catch the robber, but start() did not return -1');
-    Halt;
-  end;
+    OutputResult('FAIL', 'Cop cannot catch the robber, but start() did not return -1');
   
   if (copCorner = -1) and CopCanWin then
-  begin
-    WriteLn('FAIL');
-    WriteLn('Cop can catch the robber, but start() returned -1');
-    Halt;
-  end;
+    OutputResult('FAIL', 'Cop can catch the robber, but start() returned -1');
   
   if not CopCanWin then
-  begin
-    WriteLn('OK');
-    Halt;
-  end;
+    OutputResult('OK', '');
   
   if (copCorner < 0) or (copCorner >= N) then
-  begin
-    WriteLn('FAIL');
-    WriteLn('start() returned a value that is outside the 0..N-1 range');
-    Halt;
-  end;
+    OutputResult('FAIL', 'start() returned a value that is outside the 0..N-1 range');
   
   robberCorner := RobberStrategy[copCorner, N];
   
   if robberCorner = copCorner then
-  begin
-    WriteLn('OK');
-    Halt;
-  end;
+    OutputResult('OK', '');
   
   while True do
   begin
@@ -71,36 +71,22 @@ begin
     
     if (nextCopCorner < 0) or (nextCopCorner >= N)
       or not ((copCorner = nextCopCorner) or A[copCorner, nextCopCorner]) then
-    begin
-      WriteLn('PARTFAIL');
-      WriteLn('nextMove() returned a value that is either outside 0..N-1 ' +
-              'or the new cop position is not a neighbour to the previous one');
-      Halt;
-    end;
+        OutputResult('PARTFAIL', 'nextMove() returned a value that is either outside 0..N-1 ' +
+               'or the new cop position is not a neighbour to the previous one');
     
     copCorner := nextCopCorner;
     
     if VisitedPositions[copCorner, robberCorner] then
-    begin
-      WriteLn('PARTFAIL');
-      WriteLn('the situation repeated');
-      Halt;
-    end;
+      OutputResult('PARTFAIL', 'the situation repeated');
     
     VisitedPositions[copCorner, robberCorner] := True;
     
     if copCorner = robberCorner then
-    begin
-      WriteLn('OK');
-      Halt;
-    end;
+      OutputResult('OK', '');
     
     robberCorner := RobberStrategy[copCorner, robberCorner];
     
     if copCorner = robberCorner then
-    begin
-      WriteLn('OK');
-      Halt;
-    end;
+      OutputResult('OK', '');
   end;
 end.
