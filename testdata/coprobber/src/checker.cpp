4fb3e4c0592fe0d9cbcbe195532e5d50d6187754
Marijonas <marpetr@gmail.com>
2014-04-26 22:01:29 +0300

    Cop and robber checker: removed detailed messages

--- testdata/coprobber/src/checker.cpp
+++ testdata/coprobber/src/checker.cpp
@@ -14,13 +14,10 @@ int main(int argc, char *argv[]) {
 	
 	if (!strcmp(result, "OK")) {
 		printf("1\n");
-		fprintf(stderr, "Correct\n");
 	} else if (!strcmp(result, "PARTFAIL")) {
 		printf("0.3\n");
-		fprintf(stderr, "nextMove() failed - you will get 30%% points for this test\n");
 	} else {
 		printf("0\n");
-		fprintf(stderr, "Incorrect\n");
 	}
 	return 0;
 }
