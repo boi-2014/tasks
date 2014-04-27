59b3ff3735af550bb0a3273585fad4b9571f514a
kestas <kesvil@gmail.com>
2014-04-27 15:11:09 +0300

    More POSTMAN solutions

--- solutions/postmen/postmen-linear-light.cpp
+++ solutions/postmen/postmen-linear-light.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int MaxN = 1000010,
-	  	  MaxM = 2*1000010;
+const int MaxN = 500010,
+	  	  MaxM = 2*500010;
+
 
 int E[MaxM][3];
 int pr[MaxN] = {0};
@@ -14,47 +15,36 @@ int k[MaxM];
 
 int N, M, a, b;
 
-
-int ANS_cc[MaxM];
-int ANS_C = 0;
-int ANS_T = 0;
-int ANS[2*MaxM];
-
 bool visited[MaxN] = {0};
 vector<int> path;
-//vector< vector<int> > ANS;
 
-int RET = -1;
+int newv = -1, u;
 
 int getU (int i, int v) {
 	return (v == E[i][0]) ? E[i][1] : E[i][0];
 }
 
 
-inline void dfs(int v) {
+void dfs(int v) {
 	path.clear();
 	path.push_back(v);	
 	while (v != -1) {
 		//printf("V = %d!!!\n", v);
 		visited[v] = true;
-		int newv = -1;
+		newv = -1;
 		for (; pr[v] < C[v]; pr[v]++) {
-			int i = pr[v],
-				u = getU(k[P[v]+i], v);
-				if (E[k[P[v]+i]][2] == false) {
-					E[k[P[v]+i]][2] = true;
+			int i = k[P[v] + pr[v]];
+				if (E[i][2] == false) {
+					u = getU(i, v);
+					E[i][2] = true;
 					if (visited[u]) {
 						newv = u;
 						while (path.back() != u) {
-						        
-							ANS[ANS_T++] = path.back();
-							ANS_cc[ANS_C]++;
+						       	printf("%d ", path.back());
 							visited[path.back()] = false;
 							path.pop_back();
-						} 
-						ANS[ANS_T++] = path.back();
-						ANS_cc[ANS_C]++;
-						ANS_C++;
+						}
+						printf("%d\n", path.back());
 					}else {
 						newv = u;
 						path.push_back(u);
@@ -68,17 +58,18 @@ inline void dfs(int v) {
 		}
 		v =newv;
 	}
-	for (int i = 0; i < path.size(); i++)
-		visited[path[i]] = false;
+	visited[path[0]] = false;
+	//for (int i = 0; i < path.size(); i++)
+	//	visited[path[i]] = false;
 }
 
 
+
 int main() {
-	path.reserve(MaxN + 100);	
+	path.reserve(MaxN);	
 	scanf("%d%d\n", &N, &M);
 	for (int i = 0; i < M; i++) {
 		scanf("%d%d", &E[i][0], &E[i][1]);
-		E[i][2] = false;
 		C[E[i][0]]++;
 		C[E[i][1]]++;
 	}
@@ -88,25 +79,15 @@ int main() {
 		P[i] = P[i-1] + C[i-1];
 	}
 	for (int i = 0; i < M; i++) {
-		//printf("E: %d %d\n", E[i][0], E[i][1]);
-		k[P[E[i][0]] + tmp[E[i][0]]] = i;
-		k[P[E[i][1]] + tmp[E[i][1]]] = i;
-		tmp[E[i][0]]++;
-		tmp[E[i][1]]++;
+		a = E[i][0]; b = E[i][1];
+		k[P[a] + tmp[a]] = i;
+		k[P[b] + tmp[b]] = i;
+		tmp[a]++;
+		tmp[b]++;
 	}
 	for (int i = 1; i <= N; i++) {
 		dfs(i);
 		
 		}
-	ANS_T = 0;
-	
-	//printf("%d\n", ANS_C);
-	for(int i = 0; i < ANS_C; i++) {
-		//printf("%d", ANS_cc[i]);
-		for (int j = 0; j < ANS_cc[i]; j++)
-			printf(" %d", ANS[ANS_T++]);
-		printf("\n");
-	}
-			
 	return 0;
 }
